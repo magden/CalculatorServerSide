dc1c8cc7a120b3ab98814dace05afb3e63312808
magdem <magden2212@gmail.com>
2020-09-07 13:24:30 +0300

    added docs

--- src/main/java/com/exercise/calculator_server_side/CalculatorApplication.java
+++ src/main/java/com/exercise/calculator_server_side/CalculatorApplication.java
@@ -5,12 +5,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+/**
+ * This server-side of the Calculator application.
+ */
 @SpringBootApplication
 public class CalculatorApplication {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(CalculatorApplication.class, args);
-        //server starts to listening the port
+        //creates new sever, this server starts listening on specific port and handle requests from the client
         SocketServerHandlerImpl server = (SocketServerHandlerImpl) applicationContext.getBean(
                 "SocketServerHandler");
     }
